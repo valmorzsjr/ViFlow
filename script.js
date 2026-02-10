9a60ca7c54640b1e01f9425cb8b0a060955e4210
Valmor Junior <vzsj@hotmail.com>
2026-02-10 14:35:21 -0300

    Fix tag

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ async function buscarEMostrarVideos(){
                 <li class="videos__item">
                     <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
                     <div class="descricao-video">
-                        <img class="img-canal" src="${video.imagem} alt= "Logo do Canal">
+                        <img class="img-canal" src="${video.imagem}" alt= "Logo do Canal">
                         <h3 class="titulo-video">${video.titulo}</h3>
                         <p class="titulo-canal">${video.descricao}</p>
                         <p class="categoria" hidden>${video.categoria}</p>
